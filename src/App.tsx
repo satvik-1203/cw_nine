2b2fa956c07cc700da3962a391af79f8a0532d18
satvik-1203 <satvikgsu@gmail.com>
2024-04-07 22:43:13 -0400

    added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,109 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import FilterList from "./FilterList";
 import { products } from "./data";
 
 function App() {
   const [inputString, setInputString] = useState("");
-  const [currType, setCurrType] = useState<"Vegetables" | "Fruits" | "All">(
+  const [currType, setCurrType] = useState<"Vegetable" | "Fruit" | "All">(
     "All"
   );
   const [status, setStatus] = useState(false);
+
+  const [filteredData, setFilteredData] = useState(products);
+
+  useEffect(() => {
+    let currItems = products;
+
+    if (currType != "All") {
+      currItems = currItems.filter((item) => item.type == currType);
+    }
+
+    if (inputString.trim()) {
+      currItems = currItems.filter((item) =>
+        item.name
+          .toLowerCase()
+          .startsWith(inputString.trim().toLocaleLowerCase())
+      );
+    }
+
+    setFilteredData(currItems);
+  }, [inputString, currType]);
 
   return (
     <div className="px-4 py-4">
       <h1 className="font-bold text-2xl">Dropbox Filter</h1>
 
-      <div></div>
+      <div className="flex items-center space-x-4 my-8 justify-center">
+        <div>
+          <input
+            placeholder="Filter your options"
+            type="text"
+            className="border-2 px-4 py-2 rounded outline-gray-400"
+            onChange={(e) => {
+              setInputString(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <div
+            onClick={() => {
+              setStatus((prev) => !prev);
+            }}
+            className="px-4 py-[.65rem] relative cursor-pointer flex space-x-2 items-center text-white rounded bg-gray-400"
+          >
+            <span> Type</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+
+            <div className="absolute top-full rounded -left-2 bg-gray-500">
+              {status && (
+                <div className="rounded px-4 py-2">
+                  <div
+                    onClick={() => {
+                      setCurrType("All");
+                    }}
+                    className="px-4 py-2"
+                  >
+                    All
+                  </div>
+                  <div
+                    onClick={() => {
+                      setCurrType("Vegetable");
+                    }}
+                    className="px-4 py-2"
+                  >
+                    Vegetable
+                  </div>
+                  <div
+                    onClick={() => {
+                      setCurrType("Fruit");
+                    }}
+                    className="px-4 py-2"
+                  >
+                    Fruit
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
-        <FilterList items={products} />
+        <FilterList items={filteredData} />
       </div>
     </div>
   );
--- src/FilterList.tsx
+++ src/FilterList.tsx
@@ -7,12 +7,23 @@ interface Props {
 
 const FilterList: React.FC<Props> = ({ items }) => {
   return (
-    <div className="">
-      {items.map((item) => (
-        <div>
-          Name:{item.name}, type: {item.type}
-        </div>
-      ))}
+    <div className="flex justify-center mt-16">
+      <table className="">
+        <thead>
+          <tr>
+            <td className="p-2 border-4  font-semibold">Name</td>
+            <td className="p-2 border-4  font-semibold">Type</td>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr>
+              <td className="p-2 border-2">{item.name}</td>{" "}
+              <td className="p-2 border-2">{item.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
